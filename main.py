7901d375de32957d0a151aff9c4d0ac1d6343e07
Maxim <kmaxusk@gmail.com>
2025-03-19 19:15:04 +0200

    init

---
+++ main.py
@@ -0,0 +1,73 @@
+import urllib.parse
+import mimetypes
+import pathlib
+import json
+
+from datetime import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('index.html')
+        elif pr_url.path == '/message':
+            self.send_html_file('message.html')
+        elif pr_url.path == '/read':
+            self.send_html_file('storage\data.json')
+        else:
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file('error.html', 404)
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        print(data)
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        print(data_parse)        
+        now = datetime.now()
+        print(now)
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]} 
+        print(data_dict)
+        with open('storage\data.json', 'r+') as fh:                
+            logs = json.load(fh)
+            print(logs)
+        with open('storage\data.json', 'w') as fh:
+            logs[str(now)]=data_dict
+            print(logs)
+            fh.write(json.dumps(logs))
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
+
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ('', 3000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+if __name__ == '__main__':
+    run()
